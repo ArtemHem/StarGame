7e3de98c2bf2e7764b4f7319839a3330fd7af500
ArtemHem <sidorenko.rnd@gmail.com>
2018-11-26 22:12:36 +0300

    lesson1

---
+++ desktop/src/ru/android/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.android.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.android.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
