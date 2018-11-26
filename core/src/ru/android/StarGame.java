981f64ab346f609f055ac56d4d1b9be772c5045a
ArtemHem <sidorenko.rnd@gmail.com>
2018-11-26 22:40:19 +0300

    less1_change1

--- core/src/ru/android/StarGame.java
+++ core/src/ru/android/StarGame.java
@@ -15,7 +15,7 @@ public class StarGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("carina_2048x2048.png");
 	}
 
 	@Override
